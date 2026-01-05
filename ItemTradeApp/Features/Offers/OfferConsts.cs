accab02db3e5a7b89cdf21977f845a1f2c3ad40a
igortrsk <56490941+igortrsk@users.noreply.github.com>
2026-01-05 20:23:02 +0100

    Create OfferConsts.cs

---
+++ ItemTradeApp/Features/Offers/OfferConsts.cs
@@ -0,0 +1,9 @@
+namespace ItemTradeApp.Features.Offers;
+
+public class OfferConsts
+{
+    public const int MinBaseTokenCost = 10;
+    public const decimal BaseCostRate = 0.02m; //2%
+    public const int MinExpiryDays = 7;
+    public const int ExtraDayCost = 10;
+}
