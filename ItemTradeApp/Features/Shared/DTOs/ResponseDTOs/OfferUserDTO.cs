e8fbb3d54b4b4552fe7d788f153ff813cfcef6c6
igortrsk <56490941+igortrsk@users.noreply.github.com>
2026-01-11 17:50:57 +0100

    Update OfferUserDTO.cs

--- ItemTradeApp/Features/Shared/DTOs/ResponseDTOs/OfferUserDTO.cs
+++ ItemTradeApp/Features/Shared/DTOs/ResponseDTOs/OfferUserDTO.cs
@@ -1,3 +1,10 @@
 namespace ItemTradeApp.Features.Shared.DTOs.ResponseDTOs;
 
-public sealed record OfferUserDTO(int Id, string Nickname, string? ImageUrl, int SuccessTradesCount, float Rating, float SuccessRate);
+public sealed record OfferUserDTO(
+    int Id,
+    string Nickname,
+    string? ImageUrl,
+    int SuccessTradesCount,
+    float Rating,
+    float SuccessRate)
+    ;
