6c75cb353ec4003a8eb87cc3d225ca21ac652605
igortrsk <56490941+igortrsk@users.noreply.github.com>
2026-01-18 05:28:59 +0100

    Update OffersConsts.cs

--- ItemTradeApp/Features/Offers/OffersConsts.cs
+++ ItemTradeApp/Features/Offers/OffersConsts.cs
@@ -9,5 +9,5 @@ public class OffersConsts
     public const int PagedOffersResponseItemAmount = 2;
     public const int HighlightCost = 50;
     public const int Duration14Cost = 30;
-    public const int Duration31Cost = 31;
+    public const int Duration31Cost = 60;
 }
